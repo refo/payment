519d71f242b34b5e479a55c4742391377344abb9
Refik ONAL <refikonal@gmail.com>
2015-03-18 14:55:27 +0200

    fix method visibility

--- src/Refik/Payment/Gateway/Yapikredi.php
+++ src/Refik/Payment/Gateway/Yapikredi.php
@@ -218,7 +218,7 @@ class Yapikredi extends Gateway {
     }
 
 
-    public function saleTdsResponse()
+    protected function saleTdsResponse()
     {
         $approved = (bool)($this->xpath('approved') === '1');
 
